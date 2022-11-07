fe86b607a382dafe4a8da014c527b9e3d133db98
Leon Si <leon@leondreamed.com>
2022-11-07 11:51:17 -0500

    fix: ignore tsconfig baseUrl option

--- src/path-mapping.ts
+++ src/path-mapping.ts
@@ -18,116 +18,112 @@ export function createPathMapper(
     import('tsconfig-paths').MatchPath
   > = {};
 
-  if (compilerOptions.baseUrl) {
-    return function map(specifier: string, parentPath: string): string[] | null {
-      let tsconfigPath: string | undefined;
+  return function map(specifier: string, parentPath: string): string[] | null {
+    let tsconfigPath: string | undefined;
 
-      const filePathOfImporter = parentPath
+    const filePathOfImporter = parentPath
 
-      // Check all the existing parent folders of each known `tsconfig.json` file and see
-      // if the current file's directory falls under a known directory containing a
-      // `tsconfig.json` file
-      for (const knownTsconfigPath of Object.keys(tsconfigPathToMatchPath).sort(
-        (a, b) => a.length - b.length
-      )) {
-        if (isPathInside(filePathOfImporter, path.dirname(knownTsconfigPath))) {
-          tsconfigPath = knownTsconfigPath;
-        }
+    // Check all the existing parent folders of each known `tsconfig.json` file and see
+    // if the current file's directory falls under a known directory containing a
+    // `tsconfig.json` file
+    for (const knownTsconfigPath of Object.keys(tsconfigPathToMatchPath).sort(
+      (a, b) => a.length - b.length
+    )) {
+      if (isPathInside(filePathOfImporter, path.dirname(knownTsconfigPath))) {
+        tsconfigPath = knownTsconfigPath;
       }
+    }
 
-      // If we couldn't find an cached `tsconfig.json` which is associated with the current file, then we search for it by finding the nearest `tsconfig.json` in an above directory
-      if (tsconfigPath === undefined) {
-        const tsconfigJsonPath = findUp.sync('tsconfig.json', {
-          cwd: path.dirname(filePathOfImporter),
-        });
-        if (tsconfigJsonPath !== undefined) {
-          const config = tsConfigPaths.loadConfig(tsconfigJsonPath);
-          if (config.resultType === 'failed') {
-            throw new Error('Failed to load tsconfig');
-          }
-
-          const { absoluteBaseUrl, paths } = config;
-          let matchPath: tsConfigPaths.MatchPath;
-          if (paths === undefined) {
-            matchPath = () => undefined;
-          } else {
-            matchPath = tsConfigPaths.createMatchPath(absoluteBaseUrl, paths);
-          }
-
-          tsconfigPathToMatchPath[tsconfigJsonPath] = matchPath;
-
-          tsconfigPath = tsconfigJsonPath;
-        }
-      }
-
-      let matchPath: tsConfigPaths.MatchPath;
-      if (tsconfigPath === undefined) {
-        const config = tsConfigPaths.loadConfig();
+    // If we couldn't find an cached `tsconfig.json` which is associated with the current file, then we search for it by finding the nearest `tsconfig.json` in an above directory
+    if (tsconfigPath === undefined) {
+      const tsconfigJsonPath = findUp.sync('tsconfig.json', {
+        cwd: path.dirname(filePathOfImporter),
+      });
+      if (tsconfigJsonPath !== undefined) {
+        const config = tsConfigPaths.loadConfig(tsconfigJsonPath);
         if (config.resultType === 'failed') {
           throw new Error('Failed to load tsconfig');
         }
 
-        const { paths, absoluteBaseUrl } = config;
+        const { absoluteBaseUrl, paths } = config;
+        let matchPath: tsConfigPaths.MatchPath;
         if (paths === undefined) {
           matchPath = () => undefined;
         } else {
           matchPath = tsConfigPaths.createMatchPath(absoluteBaseUrl, paths);
         }
+
+        tsconfigPathToMatchPath[tsconfigJsonPath] = matchPath;
+
+        tsconfigPath = tsconfigJsonPath;
+      }
+    }
+
+    let matchPath: tsConfigPaths.MatchPath;
+    if (tsconfigPath === undefined) {
+      const config = tsConfigPaths.loadConfig();
+      if (config.resultType === 'failed') {
+        throw new Error('Failed to load tsconfig');
+      }
+
+      const { paths, absoluteBaseUrl } = config;
+      if (paths === undefined) {
+        matchPath = () => undefined;
       } else {
-        matchPath = tsconfigPathToMatchPath[tsconfigPath]!;
+        matchPath = tsConfigPaths.createMatchPath(absoluteBaseUrl, paths);
       }
+    } else {
+      matchPath = tsconfigPathToMatchPath[tsconfigPath]!;
+    }
 
-      const extensions: Record<string, string[]> = {
-        '.js': ['.js', '.ts'],
-        '.jsx': ['.jsx', '.tsx'],
-        '.cjs': ['.cjs', '.cts'],
-        '.mjs': ['.mjs', '.mts'],
-        '.json': ['.json']
+    const extensions: Record<string, string[]> = {
+      '.js': ['.js', '.ts'],
+      '.jsx': ['.jsx', '.tsx'],
+      '.cjs': ['.cjs', '.cts'],
+      '.mjs': ['.mjs', '.mts'],
+      '.json': ['.json']
+    }
+
+    let specifierExtension = path.parse(specifier).ext
+    let recognizedExtensions = new Set(['.js', '.ts', '.jsx', '.tsx', '.cjs', '.cts', '.mjs', '.mts', '.json'])
+    let fileMatchPath: string | undefined;
+
+    const extensionsToCheck = extensions[specifierExtension] ?? recognizedExtensions
+    const trimmedSpecifier = specifier.replace(/\.[^/.]+$/, "")
+    fileMatchPath = matchPath(trimmedSpecifier) ?? matchPath(specifier);
+
+    if (fileMatchPath === undefined) {
+      // For unknown specifiers, we use a stub
+      if (
+        !specifier.startsWith('../') &&
+        !specifier.startsWith('./') &&
+        specifier !== '.' &&
+        specifier !== '..' &&
+        specifierExtension !== '' &&
+        !recognizedExtensions.has(specifierExtension) &&
+        specifierExtension !== '.server' &&
+        specifierExtension !== '.client'
+      ) {
+        return [path.join(__dirname, '../stub.cjs')]
+      } else {
+        return null
       }
+    }
 
-      let specifierExtension = path.parse(specifier).ext
-      let recognizedExtensions = new Set(['.js', '.ts', '.jsx', '.tsx', '.cjs', '.cts', '.mjs', '.mts', '.json'])
-      let fileMatchPath: string | undefined;
+    for (const extension of extensionsToCheck) {
+      const filePath = `${fileMatchPath}${extension}`;
+      if (fs.existsSync(filePath)) {
+        return [filePath];
+      }
+    }
 
-			const extensionsToCheck = extensions[specifierExtension] ?? recognizedExtensions
-			const trimmedSpecifier = specifier.replace(/\.[^/.]+$/, "")
-			fileMatchPath = matchPath(trimmedSpecifier) ?? matchPath(specifier);
+    for (const extension of extensionsToCheck) {
+      const filePathIndex = `${fileMatchPath}/index${extension}`;
+      if (fs.existsSync(filePathIndex)) {
+        return [filePathIndex];
+      }
+    }
 
-      if (fileMatchPath === undefined) {
-				// For unknown specifiers, we use a stub
-				if (
-					!specifier.startsWith('../') &&
-					!specifier.startsWith('./') &&
-					specifier !== '.' &&
-					specifier !== '..' &&
-					specifierExtension !== '' &&
-					!recognizedExtensions.has(specifierExtension) &&
-					specifierExtension !== '.server' &&
-					specifierExtension !== '.client'
-				) {
-					return [path.join(__dirname, '../stub.cjs')]
-				} else {
-					return null
-				}
-			}
-
-			for (const extension of extensionsToCheck) {
-				const filePath = `${fileMatchPath}${extension}`;
-				if (fs.existsSync(filePath)) {
-					return [filePath];
-				}
-			}
-
-			for (const extension of extensionsToCheck) {
-				const filePathIndex = `${fileMatchPath}/index${extension}`;
-				if (fs.existsSync(filePathIndex)) {
-					return [filePathIndex];
-				}
-			}
-
-			return null
-    };
-  } else {
-    return () => null
-  }
+    return null
+  };
 }
